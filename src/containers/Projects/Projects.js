0daa42c88c3059cabf8b29dd3ec3ed97846a0a1c
jshaw14 <jshaw14@qub.ac.uk>
2018-06-01 23:47:16 +0100

    Added image to projects

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
+import Zoomy from 'react-zoomy';
 
-class componentName extends Component {
+class Projects extends Component {
     render(){
         return (
         <div>
-        <h1>This is projects</h1>    
+        <h1>This is projects</h1>   
+            <Zoomy
+              imageUrl={require('../../assets/titanic.jpg')}
+              renderThumbnail={({ showImage }) => 
+                  <img src={require('../../assets/titanic_tn.jpg')} onClick={showImage}/>}
+              scale={[1.1, 1.1]}
+              imageProps={{
+                style: {
+                  width: '100vw',
+                  height: 'auto'
+                }
+              }}
+            />
         </div>
-        )
+        );
     }
 }
 
-export default componentName;
+export default Projects;
