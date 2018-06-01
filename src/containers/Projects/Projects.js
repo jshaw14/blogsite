cb2a051d1f9e46f5c8f2f3d5ae99349d4688c0c3
jshaw14 <jshaw14@qub.ac.uk>
2018-06-01 22:35:59 +0100

    Added Projects route

---
+++ src/containers/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class componentName extends Component {
+    render(){
+        return (
+        <div>
+        <h1>This is projects</h1>    
+        </div>
+        )
+    }
+}
+
+export default componentName;
