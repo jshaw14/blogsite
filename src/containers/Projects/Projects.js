0319bc75a6a66bb1a08b1f6f2bfb561eb38c47b1
jshaw14 <jshaw14@qub.ac.uk>
2018-06-05 22:01:40 +0100

    Changed packgae.json to git pull

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import Zoomy from 'react-zoomy';
 import {ImageButton} from './Projects.style';
 import {Container} from '../../theme/grid';
 
@@ -8,7 +8,23 @@ class Projects extends Component {
         return (
         <Container>
         <h1>This is projects</h1>   
-            
+            <Zoomy
+              imageUrl={require('../../assets/titanic.jpg')}
+              renderThumbnail={({ showImage }) => 
+                  <ImageButton  onClick={showImage}>
+                    <img 
+                        src={require('../../assets/titanic_tn.jpg')}
+                        alt="titanic"/> 
+                    </ImageButton>
+                            }
+              scale={[1.1, 1.1]}
+              imageProps={{
+                style: {
+                  width: '100vw',
+                  height: 'auto'
+                }
+              }}
+            />
         </Container>
         );
     }
