0d0b00a45bc90f0b37f4242aae8922db66b87fdf
jshaw14 <jshaw14@qub.ac.uk>
2018-11-13 22:16:57 +0000

    edits to Bio

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Relative, Flex } from '../../theme/grid';
-import { Title } from './AboutMe.style'
+import { Container, Flex } from '../../theme/grid';
+import { Title, VideoContainer } from './AboutMe.style';
 import ReactPlayer from 'react-player';
-import { H2 } from '../../theme/types'
+import { H2, P } from '../../theme/types';
 
 class AboutMe extends Component {
   render() {
@@ -10,10 +10,15 @@ class AboutMe extends Component {
       <Container>
           <Title>Bio</Title>
           <H2 align="left">What's the craic?</H2>
-          <p>This website is the start of something truly special.  Maybe I'll add a video below.</p>
+          <Flex>
+          <P align="center">This website is the start of something truly special.  Maybe I'll add a video now.</P>
+          <VideoContainer>
           <ReactPlayer
             controls
             url={require('../../assets/street-performer.mp4')}/>
+           </VideoContainer> 
+            </Flex>
+            <P>Hope you liked that.  This will be a bio of my life.</P>
       </Container>
     );
   }
--- src/theme/types.js
+++ src/theme/types.js
@@ -24,12 +24,9 @@ export const A = styled.a`
     }
 `;
 
-export const H1 = styled.h1`
-    ${({align}) => align && `text-align: ${align};
+export const[H1, H2, H3, H4, H5, H6, P, Blockquote, Code]=
+['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'code']
+    .map((tag) => styled[tag]`
+    ${({ align }) => align && `text-align: ${align};
     `}
-`;
-
-export const H2 = styled.h2`
-    ${({align}) => align && `text-align: ${align};
-    `}
-`;
+`)
