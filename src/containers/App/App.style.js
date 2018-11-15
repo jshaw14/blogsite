131bc36ce443dd470dccf9b1aff7a9f2c68020ae
J Shaw <jshaw14@qub.ac.uk>
2018-11-15 09:54:04 +0000

    changed background-size to cover

--- src/containers/App/App.style.js
+++ src/containers/App/App.style.js
@@ -8,7 +8,7 @@ export const Background = styled.div`
     background-image: url('${require('../../assets/geometric.jpg')}');
     background-color: ${yellow};
     opacity: 0.2;
-    background-size: 100%;
+    background-size: cover;
     background-repeat: repeat;
     background-position: center;
     z-index: -99;
