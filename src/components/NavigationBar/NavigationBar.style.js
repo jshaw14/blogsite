cfcf03900337e73e41a09ce8f7907f13c0dc8f27
jshaw14 <jshaw14@qub.ac.uk>
2018-11-28 18:43:09 +0000

    navbar

--- src/components/NavigationBar/NavigationBar.style.js
+++ src/components/NavigationBar/NavigationBar.style.js
@@ -7,16 +7,19 @@ import media from '../../theme/media';
 export const NavigationContainer = styled(Flex)`
     top:0;
     width: 100vw;
+    height: 56px;
     position: fixed;
     z-index:1;
     opacity: 0.8;
     justify-content: flex-end;
+    background-color: white;
+    font-size: auto;
+    font-weight: bold;
 `;
 
 export const NavItem = styled(Link)`
     margin-left: 50px;
     margin-right: 50px;
-    font-size: 1.5em;
     cursor: pointer;
     color: ${black};
     position: relative;
